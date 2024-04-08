6fb8fe00a5ad0ab61326ca578d13c9a11c5b888e
ruthl7 <nemes.balazs.ferenc@students.jedlik.eu>
2024-04-08 20:26:01 +0200

    talan most jó a menu

--- Game.cs
+++ Game.cs
@@ -44,5 +44,57 @@ namespace buckshot_roulette
         {
 
         }
+        static void PlayerTurn()
+        {
+            int v;
+
+            Console.ResetColor();
+            v = Menu.MenuRajzol(new string[] { "Shoot self", "Shoot opponent", "Use item" }); // Akarunk e kilépés vagy vissza gombot?
+            switch (v)
+            {
+                case 0:
+                    Console.Clear();
+                    Console.WriteLine("ai.shotat függvény"); ;
+                    break;
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("player.shootself függvény");// Valamiért most nem működik az agyam és esélytelen hogy ezt most megcsináljam de megbeszéljük aztán holnap meglesz vagy ha akarod beírhatod a függvényeket
+                    break;
+                case 2:
+                    GetItemsList();
+                    break;
+            }
+            Console.ResetColor();
+        }
+
+
+        static void GetItemsList()
+        {
+            int v;
+            Console.ResetColor(); // egy keveset gondolkodtam azon hogy hogyan tudjuk a menübe a player tényleges itemjeit berakni de végül nem jött össze(legegyszerűbb ha csak 3 item van/többet kap a játékos és akkor a menü fix és nem random) de ez is megoldható normálisan
+            v = Menu.MenuRajzol(new string[] { "Item 1", "Item 2", "Item 3" }); // itt lehetne a vissza gomb
+            switch (v)
+            {
+                case 0:
+                    Console.Clear();
+                    Console.WriteLine("Used item 1");
+                    break;
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("used item 2");
+                    break;
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("used item 3");
+                    break;
+            }
+            Console.ResetColor();
+        }
+
+        static void AITurn()
+        {
+
+            Console.WriteLine("\nai.whotoshoot"); // ja
+        }
     }
 }
